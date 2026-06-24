03098b6feeb7217f55f52ae49445ca9f20c81241
Mayeda Tariq <mayedatariq24@gmail.com>
2026-06-24 16:42:33 +0500

    Start UI

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+string toLowerCase(string str)
+{
+    int length = str.length();
+
+    for (int i = 0; i < length; i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+    return str;
+}
+
 int main()
 {
-    
+    string start;
+    cout << "Z:\\>";
+    cin >> start;
+
+
+    while (start != "mount c c:\\8086" && start != "mount")
+
+
 
     return 0;
 }
