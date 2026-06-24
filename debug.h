bcb8e63187798786496a2d75a3c82b5078939a04
Mayeda Tariq <mayedatariq24@gmail.com>
2026-06-25 03:07:05 +0500

    Update debug

--- debug.h
+++ debug.h
@@ -21,48 +21,56 @@ void debug()
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
             case 'c':
             c();
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
             case 'e': 
             e();
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
             case 'f':
             f();
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
             case 'h':
             h();
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
             case 'm':
             m();
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
             case 'r':
             r();
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
             default:                //case 's':
             s();
             cout << "\n-";
             mode = _getch();
             cout << mode;
+            mode = tolower(mode);
             break;
         }
     }
