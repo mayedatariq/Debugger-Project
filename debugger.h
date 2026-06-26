af2d96267725e6095ac1e0680958fcb164aa59fd
Mayeda Tariq <mayedatariq24@gmail.com>
2026-06-26 07:14:36 +0500

    Update register commands

--- debugger.h
+++ debugger.h
@@ -4,7 +4,9 @@
 #include <iostream>
 #include <conio.h>
 #include "memory.h"
+#include "registers.h"
 
+//for 'h' command
 static void performArithemetic()
 {
     short int a, b;
@@ -17,6 +19,19 @@ class Debugger
 {
     private:
     Memory myMemory;
+    Register AX;
+    Register BX;
+    Register CX;
+    Register DX;
+    Register SI;
+    Register DI;
+    Register SP;
+    Register BP;
+    Register DS;
+    Register CS;
+    Register ES;
+    Register SS;
+    Register IP;
 
     public:
     //launches Debugger
@@ -90,7 +105,16 @@ class Debugger
 
                 //r for showing registers
                 case 'r':
-                //r();
+                char str[4];
+                cin.getline(str, 4);
+                if (strlen(str) == 0)
+                {
+                    displayAllRegisters();
+                }
+                else
+                {
+                    displayRegister(str);
+                }
                 cout << "\n-";
                 mode = _getch();
                 cout << mode;
@@ -108,6 +132,98 @@ class Debugger
             }
         }
     return;
+    }
+
+    //displays all registers
+    void displayAllRegisters()
+    {
+        cout << hex << uppercase << "AX=" << setw(4) << setfill('0') << AX.getValue() << "  BX=" << setw(4) << setfill('0') << BX.getValue() << "  CX=" << setw(4) << setfill('0') << CX.getValue() << "  DX=" << setw(4) << setfill('0') << DX.getValue() << "  SP=" << setw(4) << setfill('0') << SP.getValue() << "  BP=" << setw(4) << setfill('0') << BP.getValue() << "  SI=" << setw(4) << setfill('0') << SI.getValue() << "  DI=" << setw(4) << setfill('0') << DI.getValue() << '\n';
+        cout << "DS=" << setw(4) << setfill('0') << DS.getValue() << "  ES=" << setw(4) << setfill('0') << ES.getValue() << "  SS=" << setw(4) << setfill('0') << SS.getValue() << "  CS=" << setw(4) << setfill('0') << CS.getValue() << "  IP=" << setw(4) << setfill('0') << IP.getValue();
+    }
+
+    void displayRegister(char* str)
+    {
+        char registerName[3];
+        registerName[0] = tolower(str[1]);
+        registerName[1] = tolower(str[2]);
+        registerName[2] = '\0';
+
+        if (strcmp(registerName, "ax") == 0)
+        {
+            cout << "AX " << hex << setw(4) << setfill('0') << AX.getValue() << "\n:";
+            updateRegister(AX);
+        }
+        else if (strcmp(registerName, "bx") == 0)
+        {
+            cout << "BX " << hex << setw(4) << setfill('0') << BX.getValue() << "\n:";
+            updateRegister(BX);
+        }
+        else if (strcmp(registerName, "cx") == 0)
+        {
+            cout << "CX " << hex << setw(4) << setfill('0') << CX.getValue() << "\n:";
+            updateRegister(CX);
+        }
+        else if (strcmp(registerName, "dx") == 0)
+        {
+            cout << "DX " << hex << setw(4) << setfill('0') << DX.getValue() << "\n:";
+            updateRegister(DX);
+        }
+        else if (strcmp(registerName, "si") == 0)
+        {
+            cout << "SI " << hex << setw(4) << setfill('0') << SI.getValue() << "\n:";
+            updateRegister(SI);
+        }
+        else if (strcmp(registerName, "di") == 0)
+        {
+            cout << "DI " << hex << setw(4) << setfill('0') << DI.getValue() << "\n:";
+            updateRegister(DI);
+            
+        }
+        else if (strcmp(registerName, "sp") == 0)
+        {
+            cout << "SP " << hex << setw(4) << setfill('0') << SP.getValue() << "\n:";
+            updateRegister(SP);
+        }
+        else if (strcmp(registerName, "bp") == 0)
+        {
+            cout << "BP " << hex << setw(4) << setfill('0') << BP.getValue() << "\n:";
+            updateRegister(BP);
+        }
+        else if (strcmp(registerName, "ds") == 0)
+        {
+            cout << "DS " << hex << setw(4) << setfill('0') << DS.getValue() << "\n:";
+            updateRegister(DS);
+        }
+        else if (strcmp(registerName, "ss") == 0)
+        {
+            cout << "SS " << hex << setw(4) << setfill('0') << SS.getValue() << "\n:";
+            updateRegister(SS);
+        }
+        else if (strcmp(registerName, "es") == 0)
+        {
+            cout << "ES " << hex << setw(4) << setfill('0') << ES.getValue() << "\n:";
+            updateRegister(ES);
+        }
+        else if (strcmp(registerName, "cs") == 0)
+        {
+            cout << "CS " << hex << setw(4) << setfill('0') << CS.getValue() << "\n:";
+            updateRegister(CS);
+        }
+        else
+        {
+            cout << "IP " << hex << setw(4) << setfill('0') << IP.getValue() << "\n:";
+            updateRegister(IP);
+        }
+
+    }
+
+    void updateRegister(Register& r)
+    {
+        unsigned short int a;
+        cin >> hex >> a;
+        cin.ignore();
+        r.setValue(a);
+            
     }
 };
 
--- registers.h
+++ registers.h
@@ -1,6 +1,28 @@
 #ifndef REGISTERS_H
 #define REGISTERS_H
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+class Register
+{
+    private:
+    unsigned short int value;
+
+    public:
+    unsigned short int getValue()
+    {
+        return value;
+    }
+
+    void setValue(unsigned short int a)
+    {
+        value = a;
+    }
+};
 
 
 #endif
